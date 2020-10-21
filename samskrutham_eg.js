ff191fc085cd62426fc2c5bae78ce22205ec0f2d
Srinivas Gandikota <srinivas.gssn@gmail.com>
2020-10-21 22:25:42 +0530

    SM: Added framework for displaying example sentences as tooltip of vibhaktis

---
+++ samskrutham_eg.js
@@ -0,0 +1,115 @@
+
+
+
+function getVibhakthiExamples(shabdam) {
+     for (x in vibhaktiExamples) {
+         if (vibhaktiExamples[x].shabdam == shabdam)
+              return vibhaktiExamples[x];
+     }
+
+     return vibhaktiExamples[0];
+}
+//examples of shabdas
+
+
+var vibhaktiExamples = 
+
+     [
+
+         {
+              shabdam: "",
+              vibhakti: {
+               
+                  prathama: ['', '', ''],
+                  sambhodana: ['', '', ''],
+                  dvitiya: ['', '', ''],
+                  trutiya: ['', '', ''],
+                  chaturthi: ['', '', ''],
+                  panchami: ['', '', ''],
+                  shasti: ['', '', ''],
+                  saptami: ['', '', ''],
+
+              }
+         },
+
+         {
+              shabdam: "राम",
+              vibhakti: {
+               
+                  prathama: ['', '', ''],
+                  sambhodana: ['भो राम माम् उद्धरा ।'
+                             , '', ''],
+                  dvitiya: ['भक्तः रामम् नमति ।'
+                          , '', ''],
+                  trutiya: ['रामेण अभिहता निशाचरचमूः ।'
+                          , '', ''],
+                  chaturthi: ['रामाय तस्मै नमः ।'
+                            , '', ''],
+                  panchami: ['रामात् नास्ति परायणं - परतरं ।'
+                           , '', ''],
+                  shasti: ['रामस्य विमाता सुमित्रा ।'
+                         , ''
+                         , ''],
+                  saptami: ['रामे उत्तम गुणाः सन्ति ।'
+                          , ''
+                          , ''],
+
+              }
+         },
+
+         {
+              shabdam: "हरि",
+              vibhakti: {
+               
+                  prathama: ['हरिः वरम् ददाति ।'
+                           , '', ''],
+                  sambhodana: ['', '', ''],
+                  dvitiya: ['', '', ''],
+                  trutiya: ['', '', ''],
+                  chaturthi: ['', '', ''],
+                  panchami: ['', '', ''],
+                  shasti: ['', '', ''],
+                  saptami: ['', '', ''],
+
+              }
+         },
+
+         {
+              shabdam: "रमा",
+              vibhakti: {
+               
+                  prathama: ['', '', ''],
+                  sambhodana: ['', '', ''],
+                  dvitiya: ['', '', ''],
+                  trutiya: ['', '', ''],
+                  chaturthi: ['', '', ''],
+                  panchami: ['', '', ''],
+                  shasti: ['रमायाः वाहनम् गजः ।'
+                         , ''
+                         , ''],
+                  saptami: ['', '', ''],
+
+              }
+         },
+
+         {
+              shabdam: "सीता",
+              vibhakti: {
+               
+                  prathama: ['', '', ''],
+                  sambhodana: ['', '', ''],
+                  dvitiya: ['', '', ''],
+                  trutiya: ['', '', ''],
+                  chaturthi: ['', '', ''],
+                  panchami: ['', '', ''],
+                  shasti: ['सितायाः देवरः लक्ष्मणः ।'
+                         , ''
+                         , ''],
+                  saptami: [''
+                          , ''
+                          , ''],
+
+              }
+         },
+        
+     ];
